6ce42cc00216bb36e0bad6295cddec311de0f59e
scalefree <scalefree@gmail.com>
2016-08-19 16:25:01 -0500

    Update EchoServer.ino
    Values fixed for TinyDuino WiFi shield

--- examples/EchoServer/EchoServer.ino
+++ examples/EchoServer/EchoServer.ino
@@ -77,10 +77,10 @@
 #include "utility/socket.h"
 
 // These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+#define ADAFRUIT_CC3000_IRQ   2  // MUST be an interrupt pin!
 // These can be any two pins
-#define ADAFRUIT_CC3000_VBAT  5
-#define ADAFRUIT_CC3000_CS    10
+#define ADAFRUIT_CC3000_VBAT  A3
+#define ADAFRUIT_CC3000_CS    8
 // Use hardware SPI for the remaining pins
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
